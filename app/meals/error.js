f3f3050445a3b547880f8c4c605d2ba4a592d06f
Ji2mys <ji2mys@gmail.com>
2024-07-10 10:24:55 -0500

    New meals error page, remove old loading page

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error ocurred!</h1>
+      <p>Fetching of data failed. Try again later.</p>
+    </main>
+  );
+}
