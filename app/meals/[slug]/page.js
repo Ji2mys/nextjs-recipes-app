73ece7f04a345ebe8f40b51345cf3598075713c8
Ji2mys <ji2mys@gmail.com>
2024-07-10 11:18:19 -0500

    New meal details page

--- app/meals/[slug]/page.js
+++
@@ -1,8 +0,0 @@
-export default function SlugMealPage({ params }) {
-  return (
-    <main style={{ textAlign: "center", color: "white" }}>
-      <h1>Slug Meal Page</h1>
-      <p>{params.slug}</p>
-    </main>
-  );
-}
