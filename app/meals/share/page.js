a1a66ec1c53439a0593ce53af321046e59000ea0
Ji2mys <ji2mys@gmail.com>
2024-07-13 14:33:01 -0500

    Implementation of form state, share recipe form error handling implemented

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,14 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import ImagePicker from "@/components/image-picker/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +47,17 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Meal Image" name="image" />
+          {state.message && (
+            <div
+              style={{
+                background: "linear-gradient(90deg, salmon, lightsalmon)",
+                padding: "15px",
+                borderRadius: "5px",
+              }}
+            >
+              <p style={{ fontWeight: "bold", margin: 0 }}>{state.message}</p>
+            </div>
+          )}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
